cfb6025c89f876f11bc5adda8d6fc367f2ca3679
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-17 20:31:46 -0300

    feat: implements is palindrome recursive function

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    elif low_index >= high_index:
+        return word[low_index] == word[high_index]
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
