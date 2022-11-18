07e7b54c48a1f8659a6678610a800d83c82afae8
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-18 18:37:01 -0300

    fix: lint errors

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,13 @@
 def study_schedule(permanence_period, target_time):
     if type(target_time) != int:
         return None
-    
+
     most_permanence_period = 0
     for period in permanence_period:
         if type(period[0]) != int or type(period[1]) != int:
             return None
         else:
-            most_permanence_period += 1 if period[0] <= target_time <= period[1] else 0
+            most_permanence_period += 1 if (
+                period[0] <= target_time <= period[1]
+            ) else 0
     return most_permanence_period
