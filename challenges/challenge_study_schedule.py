e90ddd5ef100c4015279003a1405c8dedf2000b4
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-18 18:09:44 -0300

    feat: implements study schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) != int:
+        return None
+    
+    most_permanence_period = 0
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+        else:
+            most_permanence_period += 1 if period[0] <= target_time <= period[1] else 0
+    return most_permanence_period
+
+study_schedule([(2, 1), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5), (6, 7)], 4)
