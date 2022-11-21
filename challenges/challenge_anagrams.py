a25d6c91e389188433bc3b971af77b256943d78e
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-21 19:21:06 -0300

    feat: implements is anagram funcion

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,23 @@
+def sort_word(word):
+    word_array = list(word)
+    for i in range(len(word_array)):
+        current_value = word_array[i].lower()
+        current_position = i
+        while (current_position > 0) and (
+            word_array[current_position - 1] > current_value
+        ):
+            word_array[current_position] = word_array[current_position - 1]
+            current_position = current_position - 1
+        word_array[current_position] = current_value
+    return ''.join(word_array)
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_sorted_word = sort_word(first_string)
+    second_sorted_word = sort_word(second_string)
+    if first_string == '' or second_string == '':
+        return (first_sorted_word, second_sorted_word, False)
+    return (
+        first_sorted_word,
+        second_sorted_word,
+        first_sorted_word == second_sorted_word
+    )
