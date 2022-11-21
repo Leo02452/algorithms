a384dab6e269a2315768535d608afa3a748bcdfd
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-21 19:49:34 -0300

    feat: implements validations on find duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if (len(nums) <= 1):
+        return False
+    nums.sort()
+    for number in nums:
+        if (
+            type(number) != int
+            or number < 0
+        ):
+            return False
+    return True
