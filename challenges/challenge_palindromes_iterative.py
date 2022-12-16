122e73dc1b8aa1d60085103a0fadc37ca8c5ec48
Leo Araujo <leonardo_02452@hotmail.com>
2022-12-16 19:09:05 -0300

    feat: implements palindrome_iterative function

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+    last_index = (len(word) - 1)
+    for index in range(len(word)):
+        first_letter = word[index]
+        last_letter = word[last_index]
+
+        if first_letter == last_letter:
+            if index >= last_index:
+                return True
+            last_index -= 1
+        else:
+            return False
